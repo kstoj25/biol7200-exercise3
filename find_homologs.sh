569da33421f99ac782ef17e7050efc9f99cf61e1
kstoj25 <stojanovickatarina.keti@gmail.com>
2026-09-21 17:33:35 -0400

    new additions to find_homologs.sh

--- find_homologs.sh
+++ find_homologs.sh
@@ -4,12 +4,14 @@ query="$1"
 subject="$2"
 output="$3"
  
-makeblastdb -in "$subject" -dbtype nucl -out subject_db > /dev/null
+db_name="$(mktemp -u subject_db_XXXXXX)"
  
-tblastn -query "$query" -db subject_db \
+makeblastdb -in "$subject" -dbtype nucl -out "$db_name" > /dev/null
+ 
+tblastn -query "$query" -db "$db_name" \
     -outfmt "6 qseqid sseqid pident length qlen evalue bitscore" \
     | awk '$3 > 30 && $4 > 0.9 * $5' > "$output"
  
-rm -f subject_db.n*
+rm -f "$db_name".n*
  
 wc -l < "$output"
