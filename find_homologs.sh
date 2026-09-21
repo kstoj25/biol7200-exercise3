b1249de389491b0f68757271029280848d4f78d8
kstoj25 <stojanovickatarina.keti@gmail.com>
2026-09-21 17:31:04 -0400

    added to find_homologs.sh script
    Refactor find_homologs.sh to include database creation and improve output format.

--- find_homologs.sh
+++ find_homologs.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-tblastn -query "$1" -subject "$2" -outfmt '6 std qlen' | awk '$3 > 30 && $4 >= 0.9 * $13' > "$3"
-wc -1 < "$3"
+
+query="$1"
+subject="$2"
+output="$3"
+ 
+makeblastdb -in "$subject" -dbtype nucl -out subject_db > /dev/null
+ 
+tblastn -query "$query" -db subject_db \
+    -outfmt "6 qseqid sseqid pident length qlen evalue bitscore" \
+    | awk '$3 > 30 && $4 > 0.9 * $5' > "$output"
+ 
+rm -f subject_db.n*
+ 
+wc -l < "$output"
